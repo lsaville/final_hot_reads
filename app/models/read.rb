6183e5a2c6ef2577b4674592cb3c90dc1b35d951
Lee Saville <l.w.saville@gmail.com>
2017-02-27 17:56:04 -0700

    Add query to get the top reads from the last 24 hours

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,2 +1,8 @@
 class Read < ApplicationRecord
+  def self.hot_reads
+    select('reads.url')
+      .where('reads.created_at > ?', Time.now - 1.day)
+      .group("reads.url")
+      .order('count("reads".id) DESC').limit(10)
+  end
 end

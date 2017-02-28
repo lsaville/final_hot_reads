3d3dfb1e7ebe4203f41440223effeaeb94cf090f
Lee Saville <l.w.saville@gmail.com>
2017-02-27 18:22:39 -0700

    Add hotreads endpoint to get the hottest reads

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :reads, only: [:create]
+      get 'hotreads', to: 'current_hotreads#index'
     end
   end
 
